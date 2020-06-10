9aba63a8cc6acb6d1f8e75a15dbb24fef4a351e2
ANDY <h880702520@gmail.com>
2020-06-10 17:28:30 +0800

    changed

--- homework.py
+++ homework.py
@@ -1,24 +1,42 @@
-#請撰寫一程式，讓使用者輸入十個成績，列出由小至大的值,
-#接下來將十個成績中最小和最大值（最小、最大值不重複）以外的成績作加總及平均，並輸出結果。
-
 numberList = []
 n = int(input("請輸入列表長度(例如: 10) : "))
 for i in range(1, n+1):
     print("輸入第", i,"個數", ":")
     item = int(input())
     numberList.append(item)
+    print("=================================================================")
 print("輸出後列表 ", numberList)
-sorted_numberList =numberList
-print("排序後列表",sorted(sorted_numberList))
-print("最大值為: ", max(sorted_numberList))
+
+sorted_numberList = sorted(numberList)
+
+print("排序後列表",  (sorted_numberList))
 print("最小值為: ", min(sorted_numberList))
+print("最大值為: ", max(sorted_numberList))
 print("總和為: ", sum(sorted_numberList))
+
+total = sum(sorted_numberList) 
+i = i
+
+print("平均為:" ,total/i)
 print("=================================================================")
 
 set_numberlist = set(sorted_numberList)
 sorted_set_numberList = sorted(set_numberlist)
 
+
 print("剔除重複之數值後: ", set_numberlist)
 print("重新排序", sorted_set_numberList)
+
+x = len(sorted_set_numberList)
+print("剔除最大最小值後: ", sorted_set_numberList[1:x-1])
+
+excluded = sum(sorted_set_numberList[1:x-1])
+print("剔除後總和: ", excluded )
+
+
+y = len(sorted_set_numberList[1:x-1])
+excluded_sum = excluded/y
+print("剔除後平均: ", excluded_sum)
+
 
 
